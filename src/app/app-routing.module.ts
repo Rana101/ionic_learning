cf96691e63ff0a23a806f85656272d8a496e6cd6
Rajashekar G Naidu <raju10naidu@gmail.com>
2021-12-17 18:15:18 +0530

    fresh project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'recipes',
-    loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'recipes',
-    pathMatch: 'full'
-  }
+  // {
+  //   path: 'recipes',
+  //   loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: 'recipes',
+  //   pathMatch: 'full'
+  // }
 ];
 
 @NgModule({
